ec886c62903a8a4a6515ef29d3d2e59e3091bfee
Cestnut <hunterkudo@gmail.com>
2023-11-02 16:29:15 +0100

    submits messages to mongodb, that only accepts traffic from 10.0.0.1

--- data_gathering/data_gatherer.py
+++ data_gathering/data_gatherer.py
@@ -1,23 +1,44 @@
+import json
 import paho.mqtt.client as mqtt
+from pymongo import MongoClient
+
+def mongo_init(host, port, db_name):
+    client = MongoClient("mongodb://{}:{}".format(host,port))
+    database = client[db_name]
+    return database
+database = mongo_init("10.0.0.1", 27017, "IoT_network")
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("/data/+")
+    client.subscribe("/config/+")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-
+    if(msg.topic.startswith('/data/')):
+        device_name = msg.topic.split("/data/")[1]
+        data_collection = database["data"]
+        data = json.loads(msg.payload)
+        data["device_name"] = device_name
+        print(data)
+        data_collection.insert_one(data)
+    elif(msg.topic.startswith('/config/')):
+        device_name = msg.topic.split("/config/")[1]
+        data_collection = database["config"]
+        data = json.loads(msg.payload)
+        data["device_name"] = device_name
+        print(data)
+        data_collection.insert_one(data)
+    
 def main():
     MQTT_broker_IP = "10.0.0.1"
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-
     client.connect(MQTT_broker_IP, 1234, 60)
     client.loop_forever()
 
