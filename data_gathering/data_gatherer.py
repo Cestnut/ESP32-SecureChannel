9e5371e2cd614afc39f7765fb2eee2b51ddb4546
Cestnut <hunterkudo@gmail.com>
2023-11-13 16:21:23 +0100

    added error handling for JSONDecodeError and authentication to mongodb

--- data_gathering/data_gatherer.py
+++ data_gathering/data_gatherer.py
@@ -2,11 +2,15 @@ import json
 import paho.mqtt.client as mqtt
 from pymongo import MongoClient
 
-def mongo_init(host, port, db_name):
-    client = MongoClient("mongodb://{}:{}".format(host,port))
+def mongo_init(host, port, username, password, db_name):
+    client = MongoClient('{}:{}'.format(host, port),
+                     username=username,
+                     password=password,
+                     authSource='admin',
+                     authMechanism='SCRAM-SHA-256')
     database = client[db_name]
     return database
-database = mongo_init("10.0.0.1", 27017, "IoT_network")
+database = mongo_init("10.0.0.1", 27017, "GATHERER_USERNAME","GATHERER_PASSWORD", "IoT_network")
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -19,21 +23,26 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    if(msg.topic.startswith('/data/')):
-        device_name = msg.topic.split("/data/")[1]
-        data_collection = database["data"]
-        data = json.loads(msg.payload)
-        data["device_name"] = device_name
-        print(data)
-        data_collection.insert_one(data)
-    elif(msg.topic.startswith('/config/')):
-        device_name = msg.topic.split("/config/")[1]
-        data_collection = database["config"]
-        data = json.loads(msg.payload)
-        data["device_name"] = device_name
-        print(data)
-        data_collection.insert_one(data)
     
+    try:
+        data = json.loads(msg.payload)
+
+        if(msg.topic.startswith('/data/')):
+            device_name = msg.topic.split("/data/")[1]
+            data_collection = database["data"]
+            data["device_name"] = device_name
+            print(data)
+            data_collection.insert_one(data)
+        elif(msg.topic.startswith('/config/')):
+            device_name = msg.topic.split("/config/")[1]
+            data_collection = database["config"]
+            data["device_name"] = device_name
+            print(data)
+            data_collection.insert_one(data)
+
+    except json.decoder.JSONDecodeError as e:
+        print("An exception occurred: {}".format(e))
+
 def main():
     MQTT_broker_IP = "10.0.0.1"
     client = mqtt.Client()
