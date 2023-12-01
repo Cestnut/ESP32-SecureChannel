e6976a59507b3061e8483502038450f2fcc86f37
Cestnut <hunterkudo@gmail.com>
2023-12-01 17:00:22 +0100

    only calculates time against the first received message, hence the first received message isn't accounted for the results

--- network_benchmark/mqtt_benchmark.py
+++ network_benchmark/mqtt_benchmark.py
@@ -2,16 +2,19 @@ import json
 import paho.mqtt.client as mqtt
 import time
 import math
+import sys
 
 batch_sizes = [1,5,10,50,100]
-max_messages_number = batch_sizes[-1]
+max_messages_number = batch_sizes[-1]+1
 
-received_messages_number = 0
-first_send_timestamp = 0
-first_recv_timestamp = 0
-
-times_list = list()
-arrivals_list = list()
+def reset_globals():
+    global received_messages_number
+    global first_recv_timestamp
+    global times_list
+    
+    received_messages_number = 0
+    first_recv_timestamp = 0
+    times_list = list()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -24,43 +27,40 @@ def on_message(client, userdata, msg):
     send_timestamp = data["timestamp"]
 
     global received_messages_number
-    global first_send_timestamp
     global first_recv_timestamp
     global max_messages_number
     global batch_sizes
 
     if received_messages_number == 0:
-        first_send_timestamp = send_timestamp
         first_recv_timestamp = recv_timestamp
 
-    if received_messages_number < max_messages_number:
-        received_messages_number = received_messages_number + 1
-        arrivals_list.append(recv_timestamp)
-        if received_messages_number in batch_sizes:
-            print("{} {} {} {} {} {}".format(received_messages_number, send_timestamp, recv_timestamp, recv_timestamp-send_timestamp, recv_timestamp-first_send_timestamp, recv_timestamp - first_recv_timestamp))
-            times_list.append([recv_timestamp - first_send_timestamp, recv_timestamp - first_recv_timestamp])
+    received_messages_number = received_messages_number + 1
+    if received_messages_number-1 in batch_sizes:
+        times_list.append(recv_timestamp - first_recv_timestamp)
+
+def benchmark(output_file, mqtt_client):
+    while received_messages_number < max_messages_number: mqtt_client.loop()
+    output = ""
+    for i in range(len(times_list)):
+        output += str(times_list[i])
+        output += " "
+    output += "\n"
+    output_file.write(output)
+    print(times_list)
 
 def main():
-    MQTT_broker_IP = "10.0.0.1"
+    MQTT_broker_IP = sys.argv[1]
+    qos = sys.argv[2]
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
     client.username_pw_set("user", password="password")
     client.connect(MQTT_broker_IP, 1234, 60)
 
-    with open("{}_mqtt_benchmark_results".format(MQTT_broker_IP), "w") as output_file:
-        output_file.write("MESSAGES_NUMBER TOTAL_TIME PARTIAL_TIME\n")
-        while received_messages_number < max_messages_number: client.loop()
-        for i in range(len(times_list)):
-            output = "{} {} {}\n".format(batch_sizes[i], times_list[i][0], times_list[i][1])
-            output_file.write(output)
-        
-        latency = list()
-        for i in range(len(arrivals_list)-1):
-            latency.append(arrivals_list[i+1]-arrivals_list[i])
+    with open("{}_mqtt_qos{}_benchmark_results".format(MQTT_broker_IP, qos), "a") as output_file:
+            reset_globals()
+            benchmark(output_file, client)
 
-    print(latency)
-    print(times_list)
     client.disconnect()    
 
 if __name__ == "__main__":
